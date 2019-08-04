de7674fe74e93676aeba0a642267d1bc95023561
Vlad Leonenko <52373858+nomerphy@users.noreply.github.com>
2019-08-04 22:07:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const images = document.querySelectorAll('.anim');
+
+observer = new IntersectionObserver((entries) => {
+	entries.forEach(entry => {
+		if(entry.intersectionRatio > 0){
+		entry.target.style.animation = `anim1 2s ${entry.target.dataset.delay} forwards ease-out`;
+	} else {
+		entry.target.style.animation = 'none';
+	}
+	})
+})
+images.forEach(image => {
+	observer.observe(image);
+})
